d8bfea556791d042400d3c0041342815a527bf35
Sydney Sprouse <sydneysprouse@Sydneys-MacBook-Air.local>
2017-06-06 19:11:17 -0600

    finished

--- controller.js
+++ controller.js
@@ -1,10 +1,16 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	$scope.users = mainService.getUsers().then(function(response) {
+      console.log(1111111111, response);
+
+      $scope.users = response.data.data;
+    });
   }
 
 
   $scope.getUsers();
+
+  
 
 });
